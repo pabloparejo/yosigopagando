a603340d86d785ae6d673ddb0174269278d6c824
pabloparejo <parejo.p@gmail.com>
2020-03-18 00:25:45 +0100

    Add urls and initial content to index

--- yosigopagando/urls.py
+++ yosigopagando/urls.py
@@ -21,5 +21,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('faq/', TemplateView.as_view(template_name='faq.html'), name='faq'),
+    path('quienes-somos/', TemplateView.as_view(template_name='whoami.html'), name='whoami'),
 ]
